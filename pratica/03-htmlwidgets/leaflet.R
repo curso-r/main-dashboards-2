52db5c0eade64b55e2505def33f6757eaddbd839
williamorim <thewilliam89@gmail.com>
2022-05-04 10:37:39 -0300

    terminando exemplo leaflet

--- pratica/03-htmlwidgets/leaflet.R
+++ pratica/03-htmlwidgets/leaflet.R
@@ -4,6 +4,7 @@ library(shiny)
 library(leaflet)
 library(dplyr)
 library(reactable)
+library(sf)
 
 pnud <- readr::read_rds(here::here("dados/pnud_min.rds"))
 geo_estados <- readr::read_rds(here::here("dados/geo_estados.rds"))
@@ -13,7 +14,16 @@ ui <- fluidPage(
   hr(),
   fluidRow(
     column(
-      width = 12,
+      width = 4,
+      selectInput(
+        "ano",
+        label = "Selecione um ano",
+        choices = unique(pnud$ano),
+        width = "100%"
+      )
+    ),
+    column(
+      width = 4,
       selectInput(
         "variavel",
         label = "Selecione uma métrica",
@@ -22,7 +32,8 @@ ui <- fluidPage(
           "Expectativa de vida" = "espvida",
           "Renda per capita" = "rdpc",
           "Índice de GINI" = "gini"
-        )
+        ),
+        width = "100%"
       )
     )
   ),
@@ -45,6 +56,7 @@ server <- function(input, output, session) {
   output$mapa <- renderLeaflet({
   
     tab_mapa <- pnud |> 
+      filter(ano == input$ano) |> 
       group_by(uf_sigla) |> 
       summarise(across(input$variavel, mean, .names = "media")) |> 
       left_join(geo_estados, by = c("uf_sigla" = "abbrev_state")) |> 
@@ -59,6 +71,7 @@ server <- function(input, output, session) {
       leaflet() %>% 
       addTiles() %>% 
       addPolygons(
+        layerId = ~uf_sigla,
         fillColor = ~cores(media), 
         color = "black",
         fillOpacity = 0.8,
@@ -74,7 +87,21 @@ server <- function(input, output, session) {
       )
   })
   
-  
+  output$tabela <- renderReactable({
+    if (is.null(input$mapa_shape_click)) {
+      estado <- "RJ"
+    } else {
+      estado <- input$mapa_shape_click
+    }
+    pnud |> 
+      filter(
+        uf_sigla == estado,
+        ano == input$ano
+      ) |> 
+      select(muni_nm, one_of(input$variavel), espvida, idhm, rdpc, gini) |> 
+      arrange(across(one_of(input$variavel), desc)) |> 
+      reactable()
+  })
 }
 
 shinyApp(ui, server)
